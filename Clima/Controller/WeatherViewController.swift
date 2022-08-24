2eab2b147f852b75f14aa90e7a31a8e84f51114b
Yuliya Grygoryeva <jgrygoryeva@gmail.com>
2022-08-24 14:19:58 -0700

    search functionality

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,17 +8,46 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
     
+    @IBOutlet weak var searchText: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        searchText.delegate = self
+        
     }
 
+    @IBAction func searchPressed(_ sender: Any) {
+        searchText.endEditing(true)
+        print(searchText.text!)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchText.endEditing(true)
+        print(searchText.text!)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Type something"
+            return false
+        }
 
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        // use searchText.text to 
+        
+        searchText.text = ""
+    }
+    
+    
 }
 
