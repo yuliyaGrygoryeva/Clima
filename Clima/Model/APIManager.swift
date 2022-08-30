32e0295284dc054e78a5a92bd143b9b6428f6ed6
Yuliya Grygoryeva <jgrygoryeva@gmail.com>
2022-08-30 13:14:27 -0700

    connect UI Images to JSON data

--- Clima/Model/APIManager.swift
+++ Clima/Model/APIManager.swift
@@ -10,9 +10,8 @@ import Foundation
 
 struct APIManager {
     // add your personal key
- 
-    var weatherURL = ""
-   
+    
+    
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherURL)&q\(cityName)"
         performRequest(urlString: urlString)
@@ -26,22 +25,31 @@ struct APIManager {
             let session = URLSession(configuration: .default)
             
             // 3. Give the session a task
-            let task =  session.dataTask(with: url, completionHandler: handle(data:response:error:))
+             let task =  session.dataTask(with: url) { data, responce, error in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                if let safeData = data {
+                    parseJSON(weatherData: safeData)
+                    
+                  //  let dataString = String(data: safeData, encoding: .utf8)
+                }
+            }
             
             // 4. Start the task
             task.resume()
         }
     }
     
-    func handle(data: Data?, response: URLResponse?, error: Error?){
-        if error != nil {
-            print(error!)
-            return
+    func parseJSON(weatherData: Data) {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            print(decodedData.main.temp)
+            print(decodedData.weather[0].description)
+        } catch {
+            print(error)
         }
-        if let safeData = data {
-            let dataString = String(data: safeData, encoding: .utf8)
-        }
-        
-        
     }
 }
